e88c18b2d468f229f60300d5f35e2064c9eaa5ed
yaroslavas2001 <61469500+yaroslavas2001@users.noreply.github.com>
2021-02-09 17:50:50 +0200

    строка ввода и кнопка на js

--- js/script1.js
+++ js/script1.js
@@ -1,17 +1,14 @@
 var body=document.getElementsByTagName("body")[0];
-var table = document.createElement("table");
-var tblBody = document.createElement("tbody");
+var table = document.createElement("table"); 
 var row = document.createElement("tr");
 
-table.style.width  = '600px';
-table.style.border = '1px solid #e7c6ff';
-table.style.backgroundColor="#ffd6ff";
+table.classList.add("my");
+// добавление класс со стилями к table
 
 var td_number = document.createElement('td');
 var cellText = document.createTextNode("№");
 td_number.appendChild(cellText);
 row.appendChild(td_number);
-
 
 var td_fio = document.createElement('td');
 var cellText = document.createTextNode("ФИО");
@@ -26,3 +23,16 @@ row.appendChild(td_actions);
 table.appendChild(row);
 body.appendChild(table);
 
+//input
+var inp= document.createElement("input");
+body.appendChild(inp);
+
+// кнопка Добавить
+var btn=document.createElement("button");
+btn.textContent="Добавить"
+btn.classList.add('btn')
+btn.onclick=function(){
+    text = inp.value;
+    alert(text)
+};
+body.appendChild(btn);
