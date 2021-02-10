e21839e9a0ab7a336a5004e3cc20bac91800683e
yaroslavas2001 <61469500+yaroslavas2001@users.noreply.github.com>
2021-02-10 14:53:35 +0200

    добавление через класс на js

--- js/script1.js
+++ js/script1.js
@@ -3,6 +3,7 @@ var table = document.createElement("table");
 var row = document.createElement("tr");
 
 table.classList.add("my");
+table.id="table";
 // добавление класс со стилями к table
 
 var td_number = document.createElement('td');
@@ -23,6 +24,52 @@ row.appendChild(td_actions);
 table.appendChild(row);
 body.appendChild(table);
 
+
+class People{
+    constructor(index,text){
+        this.index=index
+        this.text=text
+    }
+    Vern(){
+        alert(this.text)
+    }
+    CreateHtml() {
+        this.table=document.getElementById("table");
+        this.row = document.createElement("tr");
+        this.table.classList.add("my");
+
+        this.td_number = document.createElement('td');
+        
+        this.td_number.innerHTML=this.index;
+        this.row.appendChild(this.td_number);
+
+        this.td_fio = document.createElement('td');
+        this.td_fio.innerHTML=this.text;
+        this.row.appendChild(this.td_fio);
+
+        this.td_actions = document.createElement("td");
+        this.btn_red=document.createElement("button");
+        this.btn_red.textContent="Редактировать";
+        this.td_actions.appendChild(this.btn_red);
+
+
+        this.btn_del=document.createElement("button");
+        this.btn_del.textContent="Удалить";
+        this.btn_del.onclick=this.Remove(this.row);
+        this.td_actions.appendChild(this.btn_del);
+
+       
+
+        this.row.appendChild(this.td_actions);
+
+        this.table.appendChild(this.row);
+        return this.table
+    }
+    Remove(event) {
+        this.row.parentNode.removeChild(this.row);
+    }
+}
+
 //input
 var inp= document.createElement("input");
 body.appendChild(inp);
@@ -31,8 +78,11 @@ body.appendChild(inp);
 var btn=document.createElement("button");
 btn.textContent="Добавить"
 btn.classList.add('btn')
+var people =1;
 btn.onclick=function(){
-    text = inp.value;
-    alert(text)
+    //text = inp.value;
+    new People(people,inp.value).CreateHtml();
+    inp.value="";
+    people+=1;
 };
 body.appendChild(btn);
