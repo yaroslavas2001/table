4e69da6fe5e2a1f1b9e81318fe7d51fd0fa183b5
yaroslavas2001 <61469500+yaroslavas2001@users.noreply.github.com>
2021-02-16 17:26:56 +0200

    редактирование

--- js/script2.js
+++ js/script2.js
@@ -25,154 +25,150 @@ table.appendChild(row);
 body.appendChild(table);
 
 
-    class People{
-        constructor(index,text){
-            this.index=index
-            this.text=text
-        }
+class People{
+    constructor(index,text){
+        this.index=index
+        this.text=text
+    }
         
-        CreateHtml() {
-            this.table=document.getElementById("table");
-            this.table.classList.add("my");
+    CreateHtml() {
+        this.table=document.getElementById("table");
+        this.table.classList.add("my");
 
-            this.TrDisplay = new TrDisplay(this.index, this.text, this.onEdit.bind(this));
-            this.TrEdit = new TrEdit(this.index, this.text);
-            this.TrEdit.Hidden();
-            //this.TrDisplay.SetData(2,"Иванов И.И.")
-            this.table.appendChild( this.TrDisplay.GetElement());
-            this.table.appendChild( this.TrEdit.GetElement());
-            
-            return this.table
-        }
+        this.TrDisplay = new TrDisplay(this.index, this.text, this.onEdit.bind(this));
+        this.TrEdit = new TrEdit(this.index, this.text);
+        this.TrEdit.Hidden();
+        //this.TrDisplay.SetData(2,"Иванов И.И.")
+        this.table.appendChild( this.TrDisplay.GetElement());
+        this.table.appendChild( this.TrEdit.GetElement());
         
-        onEdit(){
-            this.TrDisplay.Hidden();
-            this.TrEdit.Show();
-        }
-
+        return this.table
+    }    
+        
+    onEdit(){
+        this.TrDisplay.Hidden();
+        this.TrEdit.Show();
     }
 
-    class Button{
-        constructor(text, onclick){
-            this._btn=document.createElement("button");
-            this._btn.textContent=text;
-            this._btn.onclick = onclick;
-            return this._btn;
-        }
+}
+class Button{
+    constructor(text, onclick){
+        this._btn=document.createElement("button");
+        this._btn.textContent=text;
+        this._btn.onclick = onclick;
+        return this._btn;
     }
+}
 
-    class TrDisplay{
-        constructor(index, fio, onEdit){
-            this.row = document.createElement("tr");
+class TrDisplay{
+    constructor(index, fio, onEdit){
+        this.row = document.createElement("tr");
             
 
-            this.td_number = document.createElement('td');
+        this.td_number = document.createElement('td');
             
-            this.td_number.innerHTML=index;
-            this.row.appendChild(this.td_number);
+        this.td_number.innerHTML=index;
+        this.row.appendChild(this.td_number);
 
-            this.td_fio = document.createElement('td');
-            this.td_fio.innerHTML=fio;
-            this.row.appendChild(this.td_fio);
+        this.td_fio = document.createElement('td');
+        this.td_fio.innerHTML=fio;
+        this.row.appendChild(this.td_fio);
 
-            this.td_actions = document.createElement("td");
-            this.td_actions.appendChild(new Button("Edit",onEdit ));
-            this.td_actions.appendChild(new Button("Remove",this.Remove.bind(this) ));
-            this.row.appendChild(this.td_actions);
+        this.td_actions = document.createElement("td");
+        this.td_actions.appendChild(new Button("Edit",onEdit ));
+        this.td_actions.appendChild(new Button("Remove",this.Remove.bind(this) ));
+        this.row.appendChild(this.td_actions);
             
-        }
+    }
        
-        SetData(index, fio){
-            this.td_number.innerHTML=index;
-            this.td_fio.innerHTML=fio;
-        }
-        Remove(event) {
-            this.row.remove();
-           
-        }
-
-        Hidden(){
-            this.row.hidden = true;
-        }
-        Show(){
-            this.row.hidden = false;
-        }
-        GetElement(){
-            return this.row;
-        }
+    SetData(index, fio){
+        this.td_number.innerHTML=index;
+        this.td_fio.innerHTML=fio;
+    }
+    Remove(event) {
+        this.row.remove();     
     }
 
-    class TrEdit{
-        constructor(index, fio){
-            this.row = document.createElement("tr");
-            
-
-           this.td_number = document.createElement('td');
-            
-            this.td_number.innerHTML=index;
-            this.row.appendChild(this.td_number);
-            this.fio=fio;
-            this.td_fio = document.createElement('td');
-            this.inp= document.createElement("input");
-            this.td_fio.appendChild(this.inp);
-            this.row.appendChild(this.td_fio);
-
-            this.td_actions = document.createElement("td");
-            this.td_actions.appendChild(new Button("Cansel",this.Edit.bind(this,index) ));
-            this.td_actions.appendChild(new Button("Save",this.Remove.bind(this,index) ));
-            this.row.appendChild(this.td_actions);
-            console.log(index)
-        }
-        /**
-         * вавап
-         * @param {*} event 
-         * @param {*} number 
-         */
-        Edit(index) {
-            //console.log(index);
-            //this.td_fio.innerHTML=this.fio;
-            new People(index,this.fio).CreateHtml()
-            this.row.remove();
-        }
-        Remove(index) {
-            //this.td_fio.innerHTML=this.inp.value;
-            new People(index,this.inp.value).CreateHtml()
-            this.row.remove();
-            
-        }
-        Hidden(){
-            this.row.hidden = true;
-        }
-        Show(){
-            this.row.hidden = false;
-        }
-        GetElement(){
-            return this.row;
-        }
+    Hidden(){
+        this.row.hidden = true;
     }
+    Show(){
+        this.row.hidden = false;
+    }
+    GetElement(){
+        return this.row;
+    }
+}
+
+class TrEdit{
+    constructor(index, fio){
+        this.row = document.createElement("tr");
+            
+
+        this.td_number = document.createElement('td');
+            
+        this.td_number.innerHTML=index;
+        this.row.appendChild(this.td_number);
+        this.fio=fio;
+        this.td_fio = document.createElement('td');
+        this.inp= document.createElement("input");
+        this.td_fio.appendChild(this.inp);
+        this.row.appendChild(this.td_fio);
+
+        this.td_actions = document.createElement("td");
+        this.td_actions.appendChild(new Button("Cansel",this.Edit.bind(this,index) ));
+        this.td_actions.appendChild(new Button("Save",this.Remove.bind(this,index) ));
+        this.row.appendChild(this.td_actions);
+        //console.log(index)
+    }
+    /**
+    * вавап
+    * @param {*} event 
+    * @param {*} number 
+    */
+    Edit(index) {
+        //console.log(index);
+        //this.td_fio.innerHTML=this.fio;
+        new People(index,this.fio).CreateHtml()
+        this.row.remove();
+    }
+    Remove(index) {
+        //this.td_fio.innerHTML=this.inp.value;
+        new People(index,this.inp.value).CreateHtml()
+        this.row.remove();   
+    }
+    Hidden(){
+        this.row.hidden = true;
+    }
+    Show(){
+        this.row.hidden = false;
+    }
+    GetElement(){
+        return this.row;
+    }
+}
 
     
 
     
-    //input
-    var inp= document.createElement("input");
-    body.appendChild(inp);
+//input
+var inp= document.createElement("input");
+body.appendChild(inp);
 
-    // кнопка Добавить
-    var btn=document.createElement("button");
-    btn.textContent="Добавить"
-    btn.classList.add('btn')
-    var people =1;
-    btn.onclick=function(){
-        var del = new Promise(function(resolve,reject){
-            setTimeout(()=>{
-                resolve(new People(people,inp.value).CreateHtml());
-            },10)
-        });
-        del.then(function(){
-            inp.value="";
-            people+=1;
-        });
-       
-    };
-    body.appendChild(btn);
+// кнопка Добавить
+var btn=document.createElement("button");
+btn.textContent="Добавить"
+btn.classList.add('btn')
+var people =1;
+btn.onclick=function(){
+    var del = new Promise(function(resolve,reject){
+        setTimeout(()=>{
+            resolve(new People(people,inp.value).CreateHtml());
+        },10)
+    });
+    del.then(function(){
+        inp.value="";
+        people+=1;
+    });    
+};
+body.appendChild(btn);
