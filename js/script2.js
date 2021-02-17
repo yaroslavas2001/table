121b6b5147403ceb256bdb8b05c546db00e558d5
yaroslavas2001 <61469500+yaroslavas2001@users.noreply.github.com>
2021-02-17 13:00:11 +0200

    половина коментариев

--- js/script2.js
+++ js/script2.js
@@ -1,27 +1,49 @@
+// Программа с добавлением строки с номерром строки, ФИО, Кнопками редактирования
+//С помощью кнопки Добавить передатся ФИО в строку, строку можно редактировать или удалить, во время редактирования можно отменить редактирование или изменить ФИО
+
+
+
+// Нахождение в документе тег body
 var body=document.getElementsByTagName("body")[0];
-var table = document.createElement("table"); 
+//создание элемента <table></table>
+var table = document.createElement("table");
+//создание элемента <tr></tr>
 var row = document.createElement("tr");
-
-table.classList.add("my");
-table.id="table";
 // добавление класс со стилями к table
+table.classList.add("my");
+// добавление id к table
+table.id="table";
 
+//создание элемента <td></td>
 var td_number = document.createElement('td');
+// создание переменной с текстом "№"
 var cellText = document.createTextNode("№");
+// добавляем текст в td
 td_number.appendChild(cellText);
+// добавляем td в tr
 row.appendChild(td_number);
 
+//создание элемента <td></td>
 var td_fio = document.createElement('td');
+// создание переменной с текстом "ФИО"
 var cellText = document.createTextNode("ФИО");
+// добавляем текст в td
 td_fio.appendChild(cellText);
+// добавляем td в tr
 row.appendChild(td_fio);
 
+//создание элемента <td></td>
 var td_actions = document.createElement("td");
+// создание переменной с текстом "Действия"
 var cellText = document.createTextNode("Действия");
+// добавляем текст в td
 td_actions.appendChild(cellText);
+// добавляем td в tr
 row.appendChild(td_actions);
 
+// добавляем tr в table
 table.appendChild(row);
+// добавляем table в body
 body.appendChild(table);
 
 
@@ -151,24 +173,40 @@ class TrEdit{
     
 
     
-//input
+//Создаем поле для ввода ФИО
 var inp= document.createElement("input");
+//Добавляем input в body
 body.appendChild(inp);
 
-// кнопка Добавить
+//Создаем элемент <button></button>
 var btn=document.createElement("button");
+//Добавляем текст в кнопку <button>Добавить</button>
 btn.textContent="Добавить"
+//Добавляем класс к кнопке btn
 btn.classList.add('btn')
-var people =1;
+//Создаем переменную которая будет индексом
+var index =1;
+// по клику на кнопку вызываем функцию 
 btn.onclick=function(){
-    var del = new Promise(function(resolve,reject){
-        setTimeout(()=>{
-            resolve(new People(people,inp.value).CreateHtml());
-        },10)
-    });
-    del.then(function(){
-        inp.value="";
-        people+=1;
-    });    
+      /**
+    * объявляем класс
+    * @param {*} index индекс сторки
+    * @param {*} inp.value текст из строки ввода 
+    */
+    new People(index,inp.value).CreateHtml()
+    // задаем полю ввода "" чтобы стереть старый текст
+    inp.value="";
+    // увеличиваем индекс на 1
+    index+=1;
+    //var del = new Promise(function(resolve,reject){
+    //    setTimeout(()=>{
+    //        resolve(new People(people,inp.value).CreateHtml());
+    //    },10)
+    //});
+    //del.then(function(){
+    //    inp.value="";
+    //    people+=1;
+    //});    
 };
+// добавляем кнопку в body
 body.appendChild(btn);
